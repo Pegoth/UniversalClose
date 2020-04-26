5c57880df8984f1e764ec0f1955ead0f36329692
BlackSun <blacksun@hotmail.hu>
2020-04-26 17:43:03 +0200

    DebugLogger.Print will now only be called in debug mode (same as Debug.Print).

--- UniversalClose/DebugLogger.cs
+++ UniversalClose/DebugLogger.cs
@@ -5,7 +5,10 @@ namespace UniversalClose
 {
     public static class DebugLogger
     {
+        [Conditional("DEBUG")]
         public static void Print(string format, params object[] args) => Debug.Print($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}]: {string.Format(format, args)}");
+
+        [Conditional("DEBUG")]
         public static void Print(string str) => Debug.Print($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}]: {str}");
     }
 }
